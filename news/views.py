5c2f6ce0daacde40894ab9ac4ac93284ed6730f3
devkamx <devkamx@gmail.com>
2019-01-06 02:08:58 +0400

    new

--- news/views.py
+++ news/views.py
@@ -3,7 +3,14 @@ from django.http import HttpResponse
 
 # Create your views here.
 
+from .models import News
+
 def index(request):
+    news = News.objects.all()
+    context = {
+        'news': news
+    }
+
     return render(request, 'news/index.html')
 
 def news(request):
